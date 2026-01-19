42211220dcfb4734a712965080e06df01f7817e2
Javenhun733r <andrij.babyuk@gmail.com>
2026-01-19 10:29:56 +0200

    changed method calling, cause it's public

--- 18-forkify/src/js/views/addRecipeView.js
+++ 18-forkify/src/js/views/addRecipeView.js
@@ -16,7 +16,7 @@ class AddRecipeView extends View {
     this._window.classList.toggle('hidden');
   }
   _addHandlerShowWindow() {
-    this._btnOpen.addEventListener('click', this._toggleWindow.bind(this));
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
   }
   _addHandlerHideWindow() {
     this._btnClose.addEventListener('click', this._toggleWindow.bind(this));
